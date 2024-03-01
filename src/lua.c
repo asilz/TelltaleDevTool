f6655f3cc8407952e8281a1205d0f0b2e195825d
Asil Zogby <asilz@stud.ntnu.no>
2024-03-01 16:26:38 +0100

    Modified encryptLua and decryptLua
    -Mentioned functions no longer require a file size input parameter
    -Mentioned functions now require less allocated memory
    -decryptLua function now gives a warning if lua file header is wrong

--- src/lua.c
+++ src/lua.c
@@ -1,21 +1,38 @@
 #include "blowfish.h"
 #include "lua.h"
 
-int decryptLua(uint8_t *encryptedFilePath, uint8_t *decryptedFilePath, size_t fileSize)
+int decryptLua(uint8_t *encryptedFilePath, uint8_t *decryptedFilePath)
 {
-    uint64_t *buffer = malloc(fileSize);
-    FILE *ptr;
-    ptr = fopen(encryptedFilePath, "rb");
-    fread(buffer, 4, 1, ptr);
-    fread(buffer, fileSize, 1, ptr);
-    if (fclose(ptr) == EOF)
+    uint64_t *buffer = malloc(sizeof(uint64_t));
+    FILE *encryptedFile = fopen(encryptedFilePath, "rb");
+
+    fread(buffer, 4, 1, encryptedFile);
+
+    if (*buffer != 0x000000006F454C1B)
+    {
+        printf("Warning: Unexpected header in lua file %lx\n", *buffer);
+    }
+
+    FILE *decryptedFile = fopen(decryptedFilePath, "wb");
+
+    while (1)
+    {
+        size_t bytesRead = fread(buffer, 1, sizeof(uint64_t), encryptedFile);
+        if (bytesRead < sizeof(uint64_t))
+        {
+            fwrite(buffer, bytesRead, 1, decryptedFile);
+            break;
+        }
+        decryptBlock7(buffer);
+        fwrite(buffer, sizeof(uint64_t), 1, decryptedFile);
+    }
+
+    if (fclose(encryptedFile) == EOF)
     {
         return EOF;
     }
-    decryptData7(buffer, fileSize / sizeof(uint64_t));
-    ptr = fopen(decryptedFilePath, "wb");
-    fwrite(buffer, fileSize, 1, ptr);
-    if (fclose(ptr) == EOF)
+
+    if (fclose(decryptedFile) == EOF)
     {
         return EOF;
     }
@@ -23,21 +40,31 @@ int decryptLua(uint8_t *encryptedFilePath, uint8_t *decryptedFilePath, size_t fi
     return 0;
 }
 
-int encryptLua(uint8_t *decryptedFilePath, uint8_t *encryptedFilePath, size_t fileSize)
+int encryptLua(uint8_t *decryptedFilePath, uint8_t *encryptedFilePath)
 {
-    uint64_t *buffer = malloc(fileSize);
-    FILE *ptr;
-    ptr = fopen(decryptedFilePath, "rb");
-    fread(buffer, fileSize, 1, ptr);
-    if (fclose(ptr) == EOF)
+    uint64_t *buffer = malloc(sizeof(uint64_t));
+    FILE *decryptedFile = fopen(decryptedFilePath, "rb");
+    FILE *encryptedFile = fopen(encryptedFilePath, "wb");
+
+    fwrite("\x1BLEo", 4, 1, encryptedFile);
+
+    while (1)
+    {
+        size_t bytesRead = fread(buffer, 1, sizeof(uint64_t), decryptedFile);
+        if (bytesRead < sizeof(uint64_t))
+        {
+            fwrite(buffer, bytesRead, 1, encryptedFile);
+            break;
+        }
+        encryptBlock7(buffer);
+        fwrite(buffer, sizeof(uint64_t), 1, encryptedFile);
+    }
+
+    if (fclose(decryptedFile) == EOF)
     {
         return EOF;
     }
-    encryptData7(buffer, fileSize / sizeof(uint64_t));
-    ptr = fopen(encryptedFilePath, "wb");
-    fwrite("\x1BLEo", 4, 1, ptr);
-    fwrite(buffer, fileSize, 1, ptr);
-    if (fclose(ptr) == EOF)
+    if (fclose(encryptedFile) == EOF)
     {
         return EOF;
     }
